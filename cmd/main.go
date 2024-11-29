1dfef56951716f17d7027342a52c700a384fcc2b
Artem Nemurov <artem.nemurov451@gmail.com>
2024-11-29 12:46:08 +0300

    init repository

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"documentStorage"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	srv := new(documentStorage.Server)
+	if err := srv.Run(viper.GetString("port")); err != nil {
+		logrus.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
+}
