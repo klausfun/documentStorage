cd2390ad72a7ea94ee8af367bb8db358052e0baf
Artem Nemurov <artem.nemurov451@gmail.com>
2024-12-01 13:49:27 +0300

    fixed schema

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -2,6 +2,8 @@ DROP TABLE users_metadata;
 
 DROP TABLE files;
 
+DROP TABLE json_document;
+
 DROP TABLE users;
 
 DROP TABLE metadata;
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,19 +1,18 @@
 CREATE TABLE users
 (
     id            serial       not null unique,
-    login         varchar(255) not null,
+    login         varchar(255) not null unique,
     password_hash varchar(255) not null
 );
 
 CREATE TABLE metadata
 (
-    id        serial       not null unique,
-    name      varchar(255) not null,
-    file      boolean   default true,
-    public    boolean   default false,
-    mime      varchar(255) not null,
-    created   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    json_data JSONB
+    id      serial       not null unique,
+    name    varchar(255) not null,
+    file    boolean   default true,
+    public  boolean   default false,
+    mime    varchar(255) not null,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE files
@@ -23,9 +22,16 @@ CREATE TABLE files
     file_data   BYTEA                                          not null
 );
 
+CREATE TABLE json_document
+(
+    id          serial                                         not null unique,
+    metadata_id int references metadata (id) on delete cascade not null unique,
+    json_data   JSONB                                          not null
+);
+
 CREATE TABLE users_metadata
 (
-    id      serial                                           not null unique,
-    user_id int references users (id) on delete cascade      not null,
+    id          serial                                         not null unique,
+    user_id     int references users (id) on delete cascade    not null,
     metadata_id int references metadata (id) on delete cascade not null
 );
